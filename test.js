157e1f62d07a36c8bae5234ce64295c6aae77e7d
Rishi Dholkheria <rishidholkheria2001@gmail.com>
2025-05-29 19:17:46 +0530

    Sports day - logical js qstn

---
+++ test.js
@@ -0,0 +1,20 @@
+// let arr = [1,2,3,4,5]
+
+// arr.forEach((item, index) => {
+//   setTimeout(() => {
+//     console.log(item);
+//   }, index * 2000); // 2000ms = 2s
+// }); 
+
+// for (F
+
+var participants = {
+        red : 0,
+        blue : 0,
+        green : 0,
+        black : 0,
+}
+
+participants.map((item, index) => {
+    console.log(`${item} : ${index}`)   
+})
